055a3e543947335fd3cb2591ffe0671ad2c18337
Amy Dripchak <adripchak@yahoo.com>
2024-06-09 20:41:25 -0600

    update book exercises;

---
+++ Intro_Book/Functions/add.js
@@ -0,0 +1,7 @@
+function add(left, right) {
+    let sum = left + right;
+    return sum;
+}
+
+console.log(add(3, 6));
+console.log(add(3));
---
+++ Intro_Book/Functions/greeting.js
@@ -0,0 +1,19 @@
+greetPeople();
+
+function greetPeople() {
+    console.log("Good Morning!");
+}
+
+// let greetingMessage = "Good Morning!";
+//
+// function greetPeople() {
+//     console.log(greetingMessage);
+// }
+//
+// function changeGreetingMessage(newMessage) {
+//     greetingMessage = newMessage;
+// }
+//
+// changeGreetingMessage("Hello, World!");
+
+greetPeople();
