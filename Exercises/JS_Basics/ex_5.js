014d744f1572ae71707fccf166e042a0c35dbde9
Amy Dripchak <rejuvenatingserendipity@gmail.com>
2024-06-17 13:25:15 -0600

    update ex's

---
+++ Exercises/JS_Basics/ex_5.js
@@ -0,0 +1,9 @@
+let rlSync = require('readline-sync');
+let firstNumber = Number(rlSync.question('==> Enter the first number: \n'));
+let secondNumber = Number(rlSync.question('==> Enter the second number: \n'));
+console.log(`==> ${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
+console.log(`==> ${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
+console.log(`==> ${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
+console.log(`==> ${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
+console.log(`==> ${firstNumber} % ${secondNumber} = ${firstNumber % secondNumber}`);
+console.log(`==> ${firstNumber} ** ${secondNumber} = ${firstNumber ** secondNumber}`);
