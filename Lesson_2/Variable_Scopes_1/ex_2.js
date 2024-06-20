a8a5ea633c802d74cab841fc0bec7a4ae151fb7c
Amy Dripchak <adripchak@yahoo.com>
2024-06-19 20:18:40 -0600

    update

---
+++ Lesson_2/Variable_Scopes_1/ex_2.js
@@ -0,0 +1,10 @@
+let a = 'outer';
+
+function testScope() {
+    a = 'inner';
+    console.log(a);
+}
+
+console.log(a);
+testScope();
+console.log(a);
---
+++ Lesson_2/Variable_Scopes_1/ex_8.js
@@ -0,0 +1,5 @@
+console.log(a);
+
+function hello() {
+    a = 1;
+}
